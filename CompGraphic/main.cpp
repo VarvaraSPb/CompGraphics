0f06e31060cf8ce31fb4e41713635bbe29dd0942
Varya Evdokimova <4186215@gmail.com>
2025-12-04 15:41:40 +0300

    1_Wireframe
    Draw a Wireframe - first task

--- CompGraphic/main.cpp
+++ CompGraphic/main.cpp
@@ -22,7 +22,6 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
         std::swap(y0, y1);
     }
 
-
     for (int x = x0; x <= x1; x++) {
         float t = (x - x0) / (float)(x1 - x0);
         int y = y0 * (1. - t) + y1 * t;
@@ -57,7 +56,7 @@ int main(int argc, char** argv) {
         }
     }
 
-    image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    image.flip_vertically(); 
     image.write_tga_file("output.tga");
     delete model;
     return 0;
